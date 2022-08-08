64bb8a644f9601ea8379b8397e65ca9f89f19ff3
blackbeans-1301 <blackbeans.1301@gmail.com>
2022-08-08 23:26:20 +0700

    header and hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import HeaderComp from "./comp/Header/HeaderComp.js";
+import Header from './comp/Header/HeaderComp.js';
+import HeroSection from './comp/HeroSection/HeroSection';
 import HomePage from './comp/HomePage/HomePage';
 import MagicTouchGame from './comp/MagicTouch/MagicTouchGame';
 function App() {
     return (
         <div className="App">
-            <HeaderComp />
+            <Header />
+            <HeroSection />
             <Routes>
                 <Route path = "/" element = {<HomePage />} />  
                 <Route path = "/magic_touch" element = {<MagicTouchGame/>} />
--- src/comp/Header/HeaderComp.js
+++ src/comp/Header/HeaderComp.js
@@ -1,38 +1,138 @@
 import React, { useState, useContext } from "react";
 import { GlobalContext } from "../../GlobalState/GlobalContext";
+import styled from "styled-components";
 import "./Header.css";
 import logo from "../../img/logo.png";
 import { Link } from "react-router-dom";
 
-const HeaderComp = () => {
+export default function Header() {
   const GlobalState = useContext(GlobalContext);
   return (
-    <div className="header">
-      <Link
-        to={"/"}
-        id={
-          GlobalState.pageState === "Home"
-            ? "header-home-selected"
-            : "header-home-unselected"
-        }
-        onClick={() => GlobalState.setPageState("Home")}
-      >
-        <h3>Home</h3>
-      </Link>
+    <HeaderSection>
+      <HeaderWrapper>
+        <HeaderSocial>
+          <p
+            style={{
+              fontFamily: "Montserrat",
+              display: "inline-block",
+              margin: "12px 20px 5px 0",
+              alignItems: "center",
+              color: "white",
+            }}
+          >
+            Follow us:
+          </p>
+          <a className="social-icon" href="#">
+            <i className="fa fa-pinterest"></i>
+          </a>
+          <a className="social-icon" href="#">
+            <i className="fa fa-facebook"></i>
+          </a>
+          <a className="social-icon" href="#">
+            <i className="fa fa-twitter"></i>
+          </a>
+          <a className="social-icon" href="#">
+            <i className="fa fa-dribbble"></i>
+          </a>
+          <a className="social-icon" href="#">
+            <i className="fa fa-behance"></i>
+          </a>
+        </HeaderSocial>
 
-      <img id="header-logo" src={logo} alt="logo" width={100} height={80}></img>
-      <Link
-        to={"/info"}
-        id={
-          GlobalState.pageState === "Info"
-            ? "header-info-selected"
-            : "header-info-unselected"
-        }
-        onClick={() => GlobalState.setPageState("Info")}
-      >
-        <h3>Info</h3>
-      </Link>
-    </div>
+        <HeaderMenu>
+          <a href="">
+            <img className="web-logo" src={logo} alt="logo-AI"></img>
+          </a>
+
+          <nav className="navbar">
+            <ul className="main-menu primary-menu">
+              <li>
+                <a className="menu-item" href="#">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="menu-item" href="#">
+                  About us
+                </a>
+              </li>
+              <li>
+                <a className="menu-item" href="#">
+                  Reviews
+                </a>
+              </li>
+              <li>
+                <a className="menu-item" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="user-panel">
+            <a href="#">Login</a> / <a href="#">Register</a>
+          </div>
+        </HeaderMenu>
+      </HeaderWrapper>
+    </HeaderSection>
+
+    // <div className="header">
+    //   <Link
+    //     to={"/"}
+    //     id={
+    //       GlobalState.pageState === "Home"
+    //         ? "header-home-selected"
+    //         : "header-home-unselected"
+    //     }
+    //     onClick={() => GlobalState.setPageState("Home")}
+    //   >
+    //     <h3>Home</h3>
+    //   </Link>
+
+    //   <img id="header-logo" src={logo} alt="logo" width={100} height={80}></img>
+    //   <Link
+    //     to={"/info"}
+    //     id={
+    //       GlobalState.pageState === "Info"
+    //         ? "header-info-selected"
+    //         : "header-info-unselected"
+    //     }
+    //     onClick={() => GlobalState.setPageState("Info")}
+    //   >
+    //     <h3>Info</h3>
+    //   </Link>
+    // </div>
   );
-};
-export default HeaderComp;
+}
+
+const HeaderSection = styled.div`
+  padding-left: 1vw;
+  padding-right: 1vw;
+  padding-top: 1vw;
+  width: 100vw;
+  background-color: none;
+  position: absolute;
+`;
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const HeaderSocial = styled.div`
+  width: 100%;
+  height: 45px;
+  display: flex;
+  padding-right: 2px;
+  justify-content: flex-end;
+`;
+
+const HeaderMenu = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 95px;
+  background-color: #081624;
+  display: flex;
+  justify-content: space-between;
+  padding: 40px 40px 0 40px;
+  box-shadow: 0px 0px 8px #4d87f2;
+`;

4844b270451a5fc84a9b1c001358bdf685dda4aa
HUY <kk19elevanhuy@gmail.com>
2022-08-11 04:07:31 +0700

    add game 2

---
+++ src/comp/PredictFaceExpression/PredictFaceExpression.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./PredictFaceExpression.css";
+const PredictFaceExpression = () => {
+  return (
+    <div style={{margin: "0"}}>
+      <iframe
+        src="js2/index.html"
+        title="Iframe Example"
+        id="game2"
+      ></iframe>
+    </div>
+  );
+};
+
+export default PredictFaceExpression;
